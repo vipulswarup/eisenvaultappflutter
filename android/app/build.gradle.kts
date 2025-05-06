9ae5818168e07fbcb2a4e4c3d6b6249a3ed72ad4
Vipul Swarup <vipul.swarup@eisenvault.com>
2025-05-06 15:46:51 +0530

    Upgraded some packages. Not able to launch Android app yet.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 android {
     namespace = "com.eisenvault.eisenvaultappflutter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-    //ndkVersion = "27.0.12077973"  // Override the NDK version here
+    ndkVersion = "27.0.12077973"  // Using the installed NDK version
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,6 +25,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        vectorDrawables.useSupportLibrary = true
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -32,35 +34,64 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    lint {
+        disable += "InvalidPackage"
+        checkReleaseBuilds = false
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 flutter {
     source = "../.."
 }
 
-// Add this enhanced configurations block for resolutionStrategy
 configurations.all {
     resolutionStrategy {
-        // Force compatible versions of core libraries
-        force("androidx.core:core:1.9.0")
-        force("androidx.core:core-ktx:1.9.0")
-        force("androidx.window:window:1.0.0")
-        force("androidx.window:window-java:1.0.0")
-        force("androidx.activity:activity:1.6.0")
-        force("androidx.fragment:fragment:1.5.5")
-        
-        // Critical for your error - Lifecycle components
-        force("androidx.lifecycle:lifecycle-common:2.5.1")
-        force("androidx.lifecycle:lifecycle-runtime:2.5.1")
-        force("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
-        force("androidx.lifecycle:lifecycle-livedata:2.5.1")
-        force("androidx.lifecycle:lifecycle-process:2.5.1")
-        
-        // Emoji and startup components (mentioned in your error)
-        force("androidx.emoji2:emoji2:1.2.0")
-        force("androidx.emoji2:emoji2-views:1.2.0")
-        force("androidx.startup:startup-runtime:1.1.1")
-        
-        force("androidx.annotation:annotation:1.5.0")
+        force("androidx.core:core:1.8.0")
+        force("androidx.core:core-ktx:1.8.0")
+        force("androidx.appcompat:appcompat:1.4.1")
+        force("com.google.android.material:material:1.5.0")
+        force("androidx.lifecycle:lifecycle-runtime:2.3.1")
+        force("androidx.lifecycle:lifecycle-common:2.3.1")
+        force("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
+        force("androidx.lifecycle:lifecycle-livedata:2.3.1")
+        force("androidx.lifecycle:lifecycle-process:2.3.1")
+        force("androidx.lifecycle:lifecycle-service:2.3.1")
+        force("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+        force("androidx.fragment:fragment:1.3.6")
+        force("androidx.activity:activity:1.3.1")
     }
+}
+
+dependencies {
+    constraints {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.10")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    }
+
+    implementation("androidx.core:core:1.8.0")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
+    implementation("androidx.activity:activity:1.3.1")
+    implementation("androidx.fragment:fragment:1.3.6")
+    implementation("androidx.lifecycle:lifecycle-common:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-process:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-service:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.emoji2:emoji2:1.0.0")
+    implementation("androidx.emoji2:emoji2-views:1.0.0")
+    implementation("androidx.startup:startup-runtime:1.1.1")
+    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.multidex:multidex:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,16 @@
+buildscript {
+    val kotlinVersion = "1.7.10"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +18,15 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+rootProject.buildDir = File("../build")
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.buildDir = File(rootProject.buildDir, project.name)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
