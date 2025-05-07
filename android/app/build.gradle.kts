01052af48345e3726e696728600dccc47fc8464d
Vipul Swarup <vipul.swarup@eisenvault.com>
2025-05-07 12:23:57 +0530

    Refreshed android project. Works now.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.eisenvault.eisenvaultappflutter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"  // Using the installed NDK version
     //ndkVersion = flutter.ndkVersion
+     ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,78 +21,25 @@ android {
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.eisenvault.eisenvaultappflutter"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        vectorDrawables.useSupportLibrary = true
-        multiDexEnabled = true
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-
-    lint {
-        disable += "InvalidPackage"
-        checkReleaseBuilds = false
-    }
-
-    buildFeatures {
-        buildConfig = true
     }
 }
 
 flutter {
     source = "../.."
-}
-
-configurations.all {
-    resolutionStrategy {
-        force("androidx.core:core:1.8.0")
-        force("androidx.core:core-ktx:1.8.0")
-        force("androidx.appcompat:appcompat:1.4.1")
-        force("com.google.android.material:material:1.5.0")
-        force("androidx.lifecycle:lifecycle-runtime:2.3.1")
-        force("androidx.lifecycle:lifecycle-common:2.3.1")
-        force("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
-        force("androidx.lifecycle:lifecycle-livedata:2.3.1")
-        force("androidx.lifecycle:lifecycle-process:2.3.1")
-        force("androidx.lifecycle:lifecycle-service:2.3.1")
-        force("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-        force("androidx.fragment:fragment:1.3.6")
-        force("androidx.activity:activity:1.3.1")
-    }
-}
-
-dependencies {
-    constraints {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.10")
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
-    }
-
-    implementation("androidx.core:core:1.8.0")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.window:window:1.0.0")
-    implementation("androidx.window:window-java:1.0.0")
-    implementation("androidx.activity:activity:1.3.1")
-    implementation("androidx.fragment:fragment:1.3.6")
-    implementation("androidx.lifecycle:lifecycle-common:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-livedata:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-process:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-service:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.emoji2:emoji2:1.0.0")
-    implementation("androidx.emoji2:emoji2-views:1.0.0")
-    implementation("androidx.startup:startup-runtime:1.1.1")
-    implementation("androidx.annotation:annotation:1.3.0")
-    implementation("androidx.multidex:multidex:2.0.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,3 @@
-buildscript {
-    val kotlinVersion = "1.7.10"
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -18,15 +5,17 @@ allprojects {
     }
 }
 
-rootProject.buildDir = File("../build")
-subprojects {
-    project.buildDir = File(rootProject.buildDir, project.name)
-}
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
